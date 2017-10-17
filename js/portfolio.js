1c1472515e606ea14ad5cfd156568bdb06ff3193
Jona Reyes <jonamreyes@gmail.com>
2017-10-17 18:14:24 -0400

    portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,17 @@
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// typed.js
+var typed = new Typed('#typing', {
+  strings: ["thinking", "music", "autumn", "typography", "puppies","learning", "korean bbq", "adventure", "milk tea", "problem solving", "doodling", "japan", "art", "coffee"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
